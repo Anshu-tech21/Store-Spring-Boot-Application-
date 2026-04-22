046c1473dda876a9d92af4f9dc769fee0ab56813
Anshu Tingre <anshutingre2129@gmail.com>
2026-04-22 17:23:45 +0530

    Initial commit - Spring Boot project

---
+++ src/test/java/com/javawithme/Store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javawithme.Store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
